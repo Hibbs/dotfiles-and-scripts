121970846a0a1f45d1e7fe35f4db20e798d5d01e
Matt Hibbs <matthew.hibbs@gmail.com>
2013-04-11 02:17:50 -0400

    cleanup from initial commit and some prompt changes

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,2 +1,14 @@
 
+setopt prompt_subst
+
+gs_modified=${#${$(git status -z)//[^M]/}}
+gs_untracked=${#${$(git status -z)//[^?]/}}
+
+prompt_hibbs_setup () {
+  gs_modified=${#${$(git status -z)//[^M]/}}
+  gs_untracked=${#${$(git status -z)//[^?]/}}
+}
+
+
 PROMPT="%d > "
+RPROMPT="[M:$gs_modified U:$gs_untracked]"
