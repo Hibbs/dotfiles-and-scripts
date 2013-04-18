8f2fcf055b5445292311a82792ca913aa648b96e
Matt Hibbs <matthew.hibbs@gmail.com>
2013-04-18 10:19:44 -0400

    installed todo.txt CLI, added alias for todo.sh

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -7,6 +7,8 @@ alias ls1a="ls -1A"
 
 alias sniplist="sniplist.zsh"
 
+alias t='todo.sh -d ~/.todo.cfg'
+
 alias sublime='open -a Sublime\ Text\ 2'
 
 DOSBOXCONF="/Users/matthew/code/school/m_org/conf/dbconf"
